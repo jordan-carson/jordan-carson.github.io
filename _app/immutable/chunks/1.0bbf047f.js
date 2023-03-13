65f2d753cb14c4dcaac7526c04e8b26f4c477745
jordancarson <jordanlouiscarson@gmail.com>
2023-03-13 00:16:40 -0400

    removing app

--- _app/immutable/chunks/1.0bbf047f.js
+++
@@ -1,2 +0,0 @@
-import { default as t } from "../entry/_error.svelte.775a1178.js";
-export { t as component };
--- _app/immutable/chunks/4.1567ff82.js
+++
@@ -1,2 +0,0 @@
-import { default as t } from "../entry/resume-page.svelte.833446d7.js";
-export { t as component };
