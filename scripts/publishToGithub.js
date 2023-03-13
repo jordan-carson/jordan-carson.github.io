47a9ece33d7d3f01593c3033113fb6e0799ec42d
jordancarson <jordanlouiscarson@gmail.com>
2023-03-13 01:08:02 -0400

    latest push

--- scripts/publishToGithub.js
+++ scripts/publishToGithub.js
@@ -1,17 +1,27 @@
 import { publish } from 'gh-pages';
 
-publish(
-    'build', // path to public directory
-    {
-        branch: 'gh-pages',
-        repo: 'https://github.com/jordan-carson/jordan-carson.github.io.git', // Update to point to your repository
-        user: {
-            name: 'Jordan Carson', // update to use your name
-            email: 'jordanlouiscarson@gmail.com' // Update to use your email
+
+
+function deploy(branch) {
+    console.log("Deploying to branch %s", branch)
+    return publish(
+        'build', // path to public directory
+        {
+            branch: branch,
+            repo: 'git@github.com:jordan-carson/jordan-carson.github.io.git', // Update to point to your repository
+            user: {
+                name: 'Jordan Carson', // update to use your name
+                email: 'jordanlouiscarson@gmail.com' // Update to use your email
+            },
+            dotfiles: true
         },
-        dotfiles: true
-    },
-    () => {
-        console.log('Deploy Complete!');
-    }
-);
+        () => {
+            console.log('Deploy Complete!');
+        }
+    );
+}
+
+deploy("gh-pages");
+
+
+
